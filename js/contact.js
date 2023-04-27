9aed601cd238fc6efa8601935e4a8b911ae886e6
Beatriz Borges <bborges@tsf-476-wpa-0-095.epfl.ch>
2023-04-27 14:10:11 +0200

    finalized semester projects' segment

--- js/contact.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    show_hide = function() {
-        $(this).toggleClass('showing')
-        $(this).toggleClass('not-showing')
-    }
-	$('span.showing').click(show_hide)
-	$('span.not-showing').click(show_hide)
-});
