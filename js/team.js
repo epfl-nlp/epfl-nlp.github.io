f880b36f642e692f0c10f754875a4424d7420a8c
bayazitdeniz <bayazitdeniz@gmail.com>
2026-09-02 14:25:01 +0200

    fix Team img loading size issue

--- js/team.js
+++ js/team.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 	// everytime page is resized (and on load)
 	var resize_team_imgs = function(){
-		// get max-width
-		var max_width = $("section#team div.container img").first().width();
-
-		// set max-height to same value
-		$("section#team div.container img").height(max_width);
+		$("section#team img.member-photo").each(function() {
+			$(this).height($(this).parent().width());
+		});
 	};
 	resize_team_imgs();
 
